0a7d731a44483413a0ef8bc7439ce20bddd91de3
StanislavYanev <145762303+StanislavYanev@users.noreply.github.com>
2024-06-14 06:47:24 +0300

    gallery-image-update

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,10 @@
+
 from django.db import models
 
 # Create your models here.
 class GalleryImage(models.Model):
-    name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='gallery/')
+    age = models.IntegerField(blank=True)
+    name = models.CharField(max_length=100)
+    group = models.CharField(max_length=100)
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import GalleryImage
+from .forms import GalleryForm
+from django.http import HttpResponse
 
 # Create your views here.
 
 def gallery(request):
     images = GalleryImage.objects.all()
-    return render(request, 'gallery/gallery.html', {"images":images})
+    return render(request, 'gallery/gallery.html', {"images": images})
+
+
+def image_view(request):
+    if request.method == "POST":
+        form = GalleryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:gallery')
+
+    else:
+        form = GalleryForm()
+    return render(request, 'gallery/image-form.html', {'form': form})
