b7a79d22162153eb1cd1e8975f2d8cfb728bfd64
StanislavYanev <145762303+StanislavYanev@users.noreply.github.com>
2024-06-15 22:01:01 +0300

    user login logout

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('sing-in/', views.sing_in, name='sing-in'),
+    path('log-in/', views.log_in, name='log-in'),
+    path('log-out/', views.log_out, name='log-out'),
 ]
