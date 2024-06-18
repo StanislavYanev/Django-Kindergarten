c1e56aad4858e857932d01e6a30ab521ea2c5567
StanislavYanev <styanev89@gmail.com>
2024-06-18 20:38:59 +0300

    admin-panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -5,4 +5,5 @@ app_name = 'admin_panel'
 
 urlpatterns = [
     path('admin-panel/', views.admin_view, name='admin'),
+    path('admin-contact/', views.contact_view_admin, name='admin-contact'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.contrib.admin.views.decorators import staff_member_required
+
+from contact.forms import ContactForm
+from contact.models import Contact
 
 
+@staff_member_required
 def admin_view(request):
-    return  render(request, 'admin_panel/admin-panel.html')
+    return render(request, 'admin_panel/admin-panel.html')
+
+
+@staff_member_required
+def contact_view_admin(request):
+    contacts = Contact.objects.all()
+    return render(request, 'admin_panel/admin-contact.html', {"contacts": contacts})
