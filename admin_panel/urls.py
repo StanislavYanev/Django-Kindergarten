bdd46d990e79f8f272e49d0b73cf9648ee2954e2
StanislavYanev <styanev89@gmail.com>
2024-06-21 21:21:55 +0300

    admin-panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -6,4 +6,6 @@ app_name = 'admin_panel'
 urlpatterns = [
     path('admin-panel/', views.admin_view, name='admin'),
     path('admin-contact/', views.contact_view_admin, name='admin-contact'),
+    path('admin-contact/info/<int:pk>', views.contact_info_view, name='admin-contact-info'),
+    path('admin-contact-delete/<int:pk>', views.contact_delete_view, name='admin-contact-delete'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 
 from contact.forms import ContactForm
@@ -14,3 +14,19 @@ def admin_view(request):
 def contact_view_admin(request):
     contacts = Contact.objects.all()
     return render(request, 'admin_panel/admin-contact.html', {"contacts": contacts})
+
+
+@staff_member_required
+def contact_info_view(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'admin_panel/contact-details.html', {"contact": contact})
+
+
+
+@staff_member_required
+def contact_delete_view(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('admin_panel:admin')
+    return render(request, 'admin_panel/admin-contact-delete.html', {'contact': contact})
